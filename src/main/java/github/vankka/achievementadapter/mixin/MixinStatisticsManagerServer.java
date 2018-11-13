4cae084d6930ca0463c9d7989b9431e95764c1e1
Vankka <vankka.main@gmail.com>
2018-11-13 17:40:10 +0200

    Wider support & Inject instead of Overwrite

--- src/main/java/github/vankka/achievementadapter/mixin/MixinStatisticsManagerServer.java
+++ src/main/java/github/vankka/achievementadapter/mixin/MixinStatisticsManagerServer.java
@@ -1,6 +1,5 @@
 package github.vankka.achievementadapter.mixin;
 
-import github.vankka.achievementadapter.AchievementAdapter;
 import github.vankka.achievementadapter.GrantAchievementEventImplementation;
 import java.util.Set;
 import net.minecraft.entity.player.EntityPlayer;
@@ -19,6 +18,9 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = StatisticsManagerServer.class)
 public class MixinStatisticsManagerServer extends StatisticsManager {
@@ -26,48 +28,30 @@ public class MixinStatisticsManagerServer extends StatisticsManager {
     @Final @Shadow private MinecraftServer mcServer;
     @Shadow private boolean hasUnsentAchievement;
 
-    /**
-     * @author AchievementAdapter
-     * @reason GrantAchievementEvent isn't triggered normally
-     */
-    @Overwrite
-    public void unlockAchievement(EntityPlayer p_unlockAchievement_1_, StatBase p_unlockAchievement_2_, int p_unlockAchievement_3_) {
-        int lvt_4_1_ = p_unlockAchievement_2_.isAchievement() ? this.readStat(p_unlockAchievement_2_) : 0;
-
-        // AchievementAdapter start
-        if (p_unlockAchievement_2_.isAchievement() && lvt_4_1_ == 0 && p_unlockAchievement_3_ > 0) {
+    @Inject(method = "unlockAchievement", at = @At("HEAD"), cancellable = true)
+    private void onUnlockAchievement(EntityPlayer p_unlockAchievement_1_, StatBase p_unlockAchievement_2_, int p_unlockAchievement_3_, CallbackInfo callbackInfo) {
+        if (p_unlockAchievement_2_.isAchievement() && (p_unlockAchievement_2_.isAchievement() ? this.readStat(p_unlockAchievement_2_) : 0) == 0 && p_unlockAchievement_3_ > 0) {
             TextComponentTranslation textComponentTranslation = new TextComponentTranslation("chat.type.achievement", p_unlockAchievement_1_.getDisplayName(), p_unlockAchievement_2_.createChatComponent());
             Text text = TextSerializers.JSON.deserialize(TextComponentTranslation.Serializer.componentToJson(textComponentTranslation));
             Player player = Sponge.getServer().getPlayer(p_unlockAchievement_1_.getUniqueID()).orElse(null);
-            String achievement = p_unlockAchievement_2_.getStatName().getUnformattedText().toUpperCase().replace(' ', '_');
+            Achievement achievement = DummyObjectProvider.createFor(Achievement.class, p_unlockAchievement_2_.getStatName().getUnformattedText().toUpperCase().replace(' ', '_'));
 
-            GrantAchievementEventImplementation event = new GrantAchievementEventImplementation(DummyObjectProvider.createFor(Achievement.class, achievement), text, player);
+            GrantAchievementEventImplementation event = new GrantAchievementEventImplementation(achievement, text, player);
             Sponge.getEventManager().post(event);
 
-            if (event.isCancelled())
+            if (event.isCancelled()) {
+                callbackInfo.cancel();
                 return;
-
-            if (this.mcServer.isAnnouncingPlayerAchievements() && !event.isCancelled() && !event.isMessageCancelled())
-                event.getChannel().orElse(event.getOriginalChannel()).send(AchievementAdapter.class, event.getMessage());
-        }
-        // AchievementAdapter end
-
-        super.unlockAchievement(p_unlockAchievement_1_, p_unlockAchievement_2_, p_unlockAchievement_3_);
-        this.dirty.add(p_unlockAchievement_2_);
-        if (p_unlockAchievement_2_.isAchievement() && lvt_4_1_ > 0 && p_unlockAchievement_3_ == 0) {
-            this.hasUnsentAchievement = true;
-            if (this.mcServer.isAnnouncingPlayerAchievements()) {
-                this.mcServer.getPlayerList().sendChatMsg(new TextComponentTranslation("chat.type.achievement.taken", p_unlockAchievement_1_.getDisplayName(), p_unlockAchievement_2_.createChatComponent()));
             }
-        }
 
-        if (p_unlockAchievement_2_.isAchievement() && lvt_4_1_ == 0 && p_unlockAchievement_3_ > 0) {
+            if (this.mcServer.isAnnouncingPlayerAchievements() && !event.isMessageCancelled())
+                event.getChannel().orElse(event.getOriginalChannel()).send(this, event.getMessage());
+
+            super.unlockAchievement(p_unlockAchievement_1_, p_unlockAchievement_2_, p_unlockAchievement_3_);
+            this.dirty.add(p_unlockAchievement_2_);
             this.hasUnsentAchievement = true;
 
-            // AchievementAdapter
-//            if (this.mcServer.isAnnouncingPlayerAchievements()) {
-//                this.mcServer.getPlayerList().sendChatMsg(new TextComponentTranslation("chat.type.achievement", p_unlockAchievement_1_.getDisplayName(), p_unlockAchievement_2_.createChatComponent()));
-//            }
+            callbackInfo.cancel();
         }
     }
 }
